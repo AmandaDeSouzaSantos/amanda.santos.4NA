831f8e480a6004df359dbe8f2599e97f41693e59
Amanda de Souza Santos <167880554+AmandaDeSouzaSantos@users.noreply.github.com>
2025-02-20 21:12:24 +0000

    DEU ERRO PARA RODAR POREM FINALIZEI DESAFIO

--- Aula02/desafioAula02/funcoes.py
+++ Aula02/desafioAula02/funcoes.py
@@ -1,14 +1,9 @@
 def mediaLista(lista):    
-    #Lista VAzia Retorna 0    
-    if not lista:        
-        return 0    
     #Soma os valores da lista e divide pela quantidade    
     return sum(lista) / len(lista)
 
 def listaSubritaiMedia(lista):    
-    if not lista:        
-        return 0    
-    
+   
     media = mediaLista(lista)    
     desvios = []    
     
@@ -18,6 +13,82 @@ def listaSubritaiMedia(lista):
     
     #Retorna a lista     
     return desvios
+
+#Calcula media lista
+def mediaLista(lista):
+    #Soma os valores da lista e divide pela quantidade e retorna
+    return sum(lista) / len(lista)
+
+# Calcula desvios Xi - media
+def calculaDesvio(lista):
+    #Usa funcao media
+    media = mediaLista(lista)
+
+    #Cria lista para guardar desvios
+    desvios = []
+    #Percorre a lista subtrai a media e adiciona em desvios
+    for x in lista:
+        desvios.append(x - media)
+
+    #Retorna a lista
+    return desvios
+
+#Pega as listas dos desvios X * desvios Y
+def calculaDesviosXPorDesviosY(desviox, desvioy):
+    #Lista para guardar resultado
+    desviosMultiplicados = []
+    #Percorre ambas as listas e multiplica os valores correspondentes em cada indice
+    #range utiliza o tamanho de uma das listas como referência
+    #gerando a sequência de indices correta
+    for i in range(len(desvioy)):
+        desviosMultiplicados.append(desvioy[i] * desviox[i])
+    return desviosMultiplicados
+
+# Eleva desvios de X ao quadrado
+def desvioXEleva2(desviox):
+    desviosXQuadrado = []
+    for x in desviox:
+        desviosXQuadrado.append(x ** 2)
+    return desviosXQuadrado
+
+#Calcula B1
+def calculaB1(desviosMultiplicados, desviosXQuadrado):
+    return sum(desviosMultiplicados) / sum(desviosXQuadrado)
+
+#Calcula B0
+def calculaB0(b1, mediax, mediay):
+    #O B0 e a media y - b1 * media x
+    return mediay - (b1 * mediax)
+
+#Funcao principal para nao ficar um monte de import no main
+def calculaRegressaoLinear(x, y):
+    print("\nRegressão Linear\n")
+    
+    mediax = mediaLista(x)
+    mediay = mediaLista(y)
+    print(f"Media X: {mediax}")
+    print(f"Media Y: {mediay}")
+
+    desviox = calculaDesvio(x)
+    desvioy = calculaDesvio(y)
+    print(f"Desvios X: {desviox}")
+    print(f"Desvios Y: {desvioy}")
+
+    desviosMultiplicados = calculaDesviosXPorDesviosY(desviox, desvioy)
+    print(f"Desvios X * Desvios Y: {desviosMultiplicados}")
+
+    desviosXQuadrado = desvioXEleva2(desviox)
+    print(f"Desvios de X ao quadrado: {desviosXQuadrado}")
+
+    b1 = calculaB1(desviosMultiplicados, desviosXQuadrado)
+    print(f"B1 calculado: {b1}")
+
+    b0 = calculaB0(b1, mediax, mediay)
+    print(f"B0 calculado: {b0}")
+
+    return b0, b1
+
+
 
 
 
--- Aula02/desafioAula02/regressao.py
+++ Aula02/desafioAula02/regressao.py
@@ -1,4 +1,4 @@
-import funcoes
+from funcoes import calculaRegressaoLinear
 
 #Nivel Sanidade Mental
 y = [10, 7, 5, 3, 0];
@@ -6,7 +6,4 @@ y = [10, 7, 5, 3, 0];
 #Tempo Estudando MachineLearning Meses
 x = [1, 2, 3, 4, 5];
 
-mediaY = funcoes.mediaLista(y)
-mediaX = funcoes.mediaLista(x)
-
-print(mediaY)
+calculaRegressaoLinear(x, y)
